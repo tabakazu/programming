734c5b3b461e3c397fb6209f909cf3847dc34c53
tabakazu <tkazz.bd1@gmail.com>
2019-01-24 00:27:17 +0900

    fix http response message

--- scripts/http_client.rb
+++ scripts/http_client.rb
@@ -1,7 +1,14 @@
 require 'net/http'
 require 'uri'
+require 'date'
 
 ARGV.each do |url|
-  res = Net::HTTP.get_response(URI.parse(url))
-  puts res.code
+	begin
+	  res = Net::HTTP.get_response(URI.parse(url))
+		puts "HTTP/#{res.http_version} #{res.code} #{res.msg}"
+		puts "Date: #{Time.now.strftime('%Y/%m/%d %H:%M:%S')}"
+	  puts ""
+	rescue => e
+		puts e.message
+	end
 end
