a6e99edb229af9efef37a20579b41d2cd3555de6
tabakazu <tkazz.bd1@gmail.com>
2019-01-30 02:47:11 +0900

    add key bindings func

--- scripts/cui_tmp/go_demo/src/mycui/mycui.go
+++ scripts/cui_tmp/go_demo/src/mycui/mycui.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"log"
-
 	"github.com/jroimartin/gocui"
 )
 
@@ -16,7 +15,7 @@ func main() {
 
 	g.SetManagerFunc(layout)
 
-	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+	if err := initKeybindings(g); err != nil {
 		log.Panicln(err)
 	}
 
@@ -27,7 +26,7 @@ func main() {
 
 func layout(g *gocui.Gui) error {
 	maxX, maxY := g.Size()
-	if v, err := g.SetView("hello", maxX/2-7, maxY/2, maxX/2+7, maxY/2+2); err != nil {
+	if v, err := g.SetView("hello", 0, 0, maxX-1, maxY-1); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
@@ -38,4 +37,14 @@ func layout(g *gocui.Gui) error {
 
 func quit(g *gocui.Gui, v *gocui.View) error {
 	return gocui.ErrQuit
+}
+
+func initKeybindings(g *gocui.Gui) error {
+	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone,
+		func(g *gocui.Gui, v *gocui.View) error {
+			return gocui.ErrQuit
+		}); err != nil {
+		return err
+	}
+	return nil
 }
