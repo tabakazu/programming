35cdd540f1fdecb33c5cb832579f499b4e22c0eb
tabakazu <tkazz.bd1@gmail.com>
2019-02-21 01:13:11 +0900

    add case for amazon linux 2

--- chef/site-cookbooks/nginx/recipes/install.rb
+++ chef/site-cookbooks/nginx/recipes/install.rb
@@ -1,5 +1,19 @@
-package "nginx" do
-  action :install
+### Run chef-client.
+### $ bundle exec knife zero converge "name:*" -o recipe[nginx::install] --why-run
+### $ bundle exec knife zero converge "name:*" -o recipe[nginx::install]
+
+case node["platform"]
+when "amazon"
+  if node["platform_version"] == "2"
+    bash "install nginx" do
+      code "amazon-linux-extras install nginx1.12"
+      not_if { File.exists? "/usr/sbin/nginx" }
+    end
+  end
+else
+  package "nginx" do
+    action :install
+  end
 end
 
 service 'nginx' do
