e8473ef95abeb189a31c94e9d494ab827933f28d
tabakazu <tkazz.bd1@gmail.com>
2019-06-15 22:04:53 +0900

    Fixed directly structure

--- lambda/python3.6/http_request.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-def lambda_handler(event, context):
-    uri = 'https://www.google.co.jp'
-    res = requests.get(uri)
-    print(res.status_code)
-    return res.status_code
--- lambda/ruby2.5/http_request.rb
+++
@@ -1,9 +0,0 @@
-require 'net/http'
-
-def lambda_handler(event:, context:)
-  uri = 'https://www.google.co.jp'
-  res = Net::HTTP.get_response(URI.parse(uri))
-  puts res.code
-  res.code
-end
-
