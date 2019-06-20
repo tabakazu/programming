4ca4237b60c9438d3dc739dc1e37b5bab53f917c
tabakazu <tkazz.bd1@gmail.com>
2019-06-20 23:11:26 +0900

    Rubyかけない中年

--- gomi_codes/fibonacci.rb
+++ gomi_codes/fibonacci.rb
@@ -1,6 +1,9 @@
-def fibo(n)
-  n < 2 ? n : fibo(n - 1) + fibo(n - 2)
+def rec(f2, f1, n)
+  n == 1 ? f1 : rec(f1, f2 + f1, n - 1)
 end
 
-puts fibo(10)
-# 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
+def fibo(n)
+  n < 2 ? n : rec(0, 1, n)
+end
+
+puts fibo(1000)
