54d273cf72d53e7d9ec92776e3d38061e3fcd4dc
tabakazu <tkazz.bd1@gmail.com>
2019-07-06 22:59:08 +0900

    add OpenStruct sample

--- gomi_codes/sandbox.rb
+++ gomi_codes/sandbox.rb
@@ -18,6 +18,7 @@ puts arr.class, arr
 hash = { a: 1, b: 2 }
 puts hash.class, hash
 
+
 # 構造体クラス
 ## クラス名あり ※ 第一引数がクラス名、keyword_init が true でキーワード引数有効
 person_struct = Struct.new('Person', :name, :age, keyword_init: true)
@@ -28,3 +29,10 @@ puts person1.class, person1.name, person1.age
 Person = Struct.new(:name, :age)
 person2 = Person.new('hanako', 40)
 puts person2.class, person2.name, person2.age
+
+
+# 要素を動的に追加・削除できる構造体クラス
+require 'ostruct'
+o_struct = OpenStruct.new({ name: 'pochi' })
+o_struct.age = 10 # 初期値にない要素を追加可能
+puts o_struct.class, o_struct.name, o_struct.age
