f7cc1a188ea15512184d35e006425ad00632502a
tabakazu <tkazz.bd1@gmail.com>
2019-07-10 00:57:59 +0900

    add gorm sample

---
+++ gomi_codes/ormdemo/gormdemo/README.md
@@ -0,0 +1,5 @@
+# GORM Demo
+```
+$ go get -u github.com/go-sql-driver/mysql
+$ go get -u github.com/jinzhu/gorm
+```
---
+++ gomi_codes/ormdemo/gormdemo/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+)
+
+type Post struct {
+	gorm.Model
+	Title   string `gorm:"not null" form:"title" json:"title"`
+	Content string `gorm:"not null" form:"content" json:"content" sql:"type:text;"`
+}
+
+func CreateDb() error {
+	db, err := sql.Open("mysql", "root:@/")
+	defer db.Close()
+
+	if err != nil {
+		return err
+	}
+	_, err = db.Exec("CREATE DATABASE IF NOT EXISTS gormdemo_development")
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func MigrateSchema(db *gorm.DB) error {
+	if err := db.AutoMigrate(&Post{}).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func init() {
+	if err := CreateDb(); err != nil {
+		panic(err.Error())
+	}
+}
+
+type PostRepo struct{}
+
+func newPostRepo() *PostRepo {
+	return new(PostRepo)
+}
+
+func (r PostRepo) CreatePost(post Post, db *gorm.DB) error {
+	if err := db.Create(&post).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func main() {
+	db, err := gorm.Open("mysql", "root:@/gormdemo_development?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+
+	if err := MigrateSchema(db); err != nil {
+		panic(err.Error())
+	}
+
+	repo := newPostRepo()
+	post := Post{Title: "Post 1", Content: "Body 1"}
+	if err := repo.CreatePost(post, db); err != nil {
+		panic(err.Error())
+	}
+}
