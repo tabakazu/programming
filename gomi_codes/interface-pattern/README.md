481ab42bbaca4b06affe3c34143c039f37360377
tabakazu <tkazz.bd1@gmail.com>
2019-07-21 13:58:59 +0900

    add db connect setting

---
+++ gomi_codes/interface-pattern/README.md
@@ -0,0 +1,9 @@
+init sql
+```
+CREATE DATABASE IF NOT EXISTS simple_rest_api_dev;
+```
+
+run app
+```
+go run main.go
+```
--- gomi_codes/interface-pattern/main.go
+++ gomi_codes/interface-pattern/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+type Db struct {
+	Conn *gorm.DB
+}
+
+func NewDb() *Db {
+	conn, err := gorm.Open("mysql", "root:@/simple_rest_api_dev?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		panic(err.Error())
+	}
+	db := new(Db)
+	db.Conn = conn
+	db.Conn.AutoMigrate(&Post{})
+	return db
+}
+
+type Post struct {
+	gorm.Model
+	Title string `gorm:"not null" form:"title" json:"title"`
+	Body  string `gorm:"not null" form:"body" json:"body"`
+}
 
 func main() {
+	db := NewDb()
+	conn := db.Conn
+	fmt.Println(conn)
+
 	fmt.Println("Hello Interface Pattern")
 }
