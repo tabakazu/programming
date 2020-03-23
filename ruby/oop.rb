9a6fbb4b085378c2b6275866c982b3c085ecd989
tabakazu <tkazz.bd1@gmail.com>
2020-03-24 00:23:10 +0900

    add template pattern

--- ruby/oop.rb
+++ ruby/oop.rb
@@ -28,3 +28,36 @@ corporate_customer_1 = CorporateCustomer.new({ name: '株式会社ABC', capital:
 
 p individual_customer_1
 p corporate_customer_1
+
+
+###
+### ポリモーフィズム(多様性)
+###
+class Customer # 顧客
+  # テンプレートパターン
+  def info
+    raise NotImplementedError, "This #{self.class} cannot respond to:"
+  end
+end
+
+class IndividualCustomer < Customer # 個人顧客
+  def info
+    { name: @name, sex: @sex }
+  end
+end
+
+class CorporateCustomer < Customer # 法人顧客
+  def info
+    { name: @name, capital: @capital }
+  end
+end
+
+def get_info_by_customer customer
+  customer.info
+end
+
+individual_customer_2 = IndividualCustomer.new({ name: 'Bob', sex: 'male' })
+corporate_customer_2 = CorporateCustomer.new({ name: 'あいうえお株式会社', capital: 1_000 })
+
+p get_info_by_customer(individual_customer_2)
+p get_info_by_customer(corporate_customer_2)
