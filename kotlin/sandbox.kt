bb3e194527959959f437172628a4102905e80ea8
tabakazu <tkazz.bd1@gmail.com>
2020-03-30 23:03:50 +0900

    add class, interface and generics sample

--- kotlin/sandbox.kt
+++ kotlin/sandbox.kt
@@ -1,5 +1,52 @@
+//
 // クラス
-class Person(var name: String) {
+//
+class Person(val firstName: String, val lastName: String) {
+  fun fullName(): String {
+    return "${this.firstName} ${this.lastName}"
+  }
+}
+
+//
+// インターフェース
+//
+interface IPersonRepository {
+  fun findByFirstName(firstName: String): Person
+}
+// インターフェースの実装
+class PersonRepository : IPersonRepository {
+  override fun findByFirstName(firstName: String): Person {
+    val person = Person(firstName, "Smith")
+    return person
+  }
+}
+// インターフェースの実装を呼び出すクラス
+class PersonUsecase(val repo: IPersonRepository) {
+  fun getPersonByFirstName(firstName: String): Person {
+    val person = repo.findByFirstName(firstName)
+    return person
+  }
+}
+
+//
+// ジェネリクス
+//
+interface IGenericsRepository<T> {
+  fun first(): T
+}
+// インターフェースの実装
+class PersonGenericsRepository : IGenericsRepository<Person> {
+  override fun first(): Person {
+    val person = Person("1st", "Person")
+    return person
+  }
+}
+// インターフェースの実装を呼び出すクラス
+class PersonGenericsUsecase(val repo: IGenericsRepository<Person>) {
+  fun getFirstPerson(): Person {
+    val person = repo.first()
+    return person
+  }
 }
 
 fun main(args: Array<String>) {
@@ -22,8 +69,28 @@ fun main(args: Array<String>) {
   println("${i} is ${i.javaClass.name}")
   println("${str} is ${str.javaClass.name}")
 
+  // if 式
+  val flag: Boolean = true
+  if (flag)
+    println("flag is true")
+  else 
+    println("flag is false")
+
   // クラスのインスタンス生成
-  val person = Person("Joe Smith")
+  val person = Person("Joe", "Smith")
   println("${person} is ${person.javaClass.name}")
-  println("person.name is ${person.name}")
+  println("person.firstName is ${person.firstName}")
+  println("person.fullName is ${person.fullName()}")
+
+  // インターフェースの実装 & ダックタイピング
+  val repo = PersonRepository()
+  val usecase = PersonUsecase(repo)
+  val person1 = usecase.getPersonByFirstName("Alice")
+  println(person1.firstName)
+
+  // ジェネリクス
+  val repo2 = PersonGenericsRepository()
+  val usecase2 = PersonGenericsUsecase(repo2)
+  val person2 = usecase2.getFirstPerson()
+  println(person2.fullName())
 }
