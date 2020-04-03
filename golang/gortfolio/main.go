b98f7e104a2835ecd2d5c61a57774c0494285efa
tabakazu <tkazz.bd1@gmail.com>
2020-04-03 22:55:36 +0900

    add base file

---
+++ golang/gortfolio/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	// Ping test
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(http.StatusOK, "pong")
+	})
+
+	r.Run(":8080")
+
+	fmt.Println("Go Application starting...")
+}
