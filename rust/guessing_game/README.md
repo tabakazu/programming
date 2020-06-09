9eb88cb291219c75a4845a8bffdf23c7b5dff730
tabakazu <tkazz.bd1@gmail.com>
2020-06-10 01:17:15 +0900

    add guessing_game of tutorial

---
+++ rust/guessing_game/README.md
@@ -0,0 +1,17 @@
+# guessing_game - Rust Tutorial
+
+## Use Docker
+```bash
+$ docker-compose build
+$ docker-compose run --rm --service-ports app bash
+```
+
+## Create a new project
+```bash
+docker > cargo init
+```
+
+## Run app
+```bash
+docker > cargo run
+```
---
+++ rust/guessing_game/src/main.rs
@@ -0,0 +1,36 @@
+extern crate rand;
+
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    println!("数字を当ててみて!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    loop {
+        println!("予想値を入力して下さい");
+
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess)
+            .expect("行の読み取りに失敗しました");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("あなたの予想値: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less    => println!("小さすぎます!"),
+            Ordering::Greater => println!("大きすぎます!"),
+            Ordering::Equal   => {
+                println!("あなたの勝ちです!");
+                break;
+            }
+        }
+    }
+}
