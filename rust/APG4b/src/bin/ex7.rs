2a19a57d7591819003021052da5dc730c3ceb933
tabakazu <tkazz.bd1@gmail.com>
2020-06-23 00:20:07 +0900

    add ex7

---
+++ rust/APG4b/src/bin/ex7.rs
@@ -0,0 +1,27 @@
+// $ cargo run --bin ex7
+
+fn main() {
+  let a = true;
+  let b = false;
+  let c = true;
+
+  if a {
+    print!("At");
+  } else {
+    print!("Yo");
+  }
+
+  if !a || b {
+    print!("Bo");
+  } else if !b || c {
+    print!("Co");
+  }
+
+  if a && b && c {
+    println!("foo");
+  } else if true && false {
+    println!("yeah!");
+  } else if !a || c {
+    println!("der");
+  }
+}
