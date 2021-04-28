5fec9e0cd7df8624b95c0b3e614b73a15c9c1e0c
tabakazu <tkazz.bd1@gmail.com>
2021-04-29 00:11:42 +0900

    add test code

--- rust/oop-demo/src/user.rs
+++ rust/oop-demo/src/user.rs
@@ -9,3 +9,13 @@ impl User {
         [self.family_name.clone(), self.given_name.clone()].join("")
     }
 }
+
+#[test]
+fn it_returns_full_name() {
+    let user = User {
+        email: "john_snow@example.com".to_string(),
+        family_name: "John".to_string(),
+        given_name: "Snow".to_string(),
+    };
+    assert_eq!(user.full_name(), "JohnSnow");
+}
