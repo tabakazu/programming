424dcaea324ad2931718e5f88fe89a5d8112279e
tabakazu <tkazz.bd1@gmail.com>
2021-05-10 01:31:12 +0900

    add `IF NOT EXISTS` option

--- sql/seed/sample_mysql_8_0.sql
+++ sql/seed/sample_mysql_8_0.sql
@@ -2,7 +2,7 @@ CREATE DATABASE IF NOT EXISTS sandbox;
 
 USE sandbox;
 
-CREATE TABLE `items` (
+CREATE TABLE IF NOT EXISTS `items` (
   `id` bigint unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL DEFAULT '' COMMENT 'タイトル',
   `slug` varchar(255) NOT NULL DEFAULT '' COMMENT 'スラッグ',
